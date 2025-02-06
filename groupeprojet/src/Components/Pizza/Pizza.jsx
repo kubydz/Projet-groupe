0b3a259ead8fec8632d05682cc9201c65cc3424e
kubydz <kubydonmez@icloud.com>
2025-02-06 21:39:48 +0100

    Cards clean

--- groupeprojet/src/Components/Pizza/Pizza.jsx
+++ groupeprojet/src/Components/Pizza/Pizza.jsx
@@ -6,7 +6,7 @@ import QuatreFromages from '../../assets/img/4fromages.jpg';
 import Royale from '../../assets/img/royale.png';
 import Vege from '../../assets/img/vege.jpg';
 import Provencale from '../../assets/img/pizza-provencale.jpg';
-import Italienne from '../../assets/img/pizza-italienne.avif';
+import Italienne from '../../assets/img/pizzaita.avif';
 import Espagnole from '../../assets/img/espagnole.jpg';
 import Turque from '../../assets/img/pide.avif';
 import Peperoni from '../../assets/img/peperroni.jpg';
@@ -22,10 +22,11 @@ function Pizza(params) {
         <link rel="stylesheet" href="./public/styles.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://fonts.googleapis.com/css2?family=Barriecito&display=swap" rel="stylesheet" />
         <header>
             <div className="opacite">
-                <h1>Pizza de Baba Laulaukubz Pizza</h1>
+                <h1>π de Baba Laulaukubz Pizza</h1>
                 <section className="finance" />
             </div>
         </header>
@@ -39,6 +40,7 @@ function Pizza(params) {
                 <ul>
                     <li>Tomates, mozzarella, basilic frais, sel, huile d'olive</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             {/* Répétez les cartes suivantes pour les autres produits */}
             <div className="card">
@@ -49,6 +51,7 @@ function Pizza(params) {
                 <ul>
                     <li>Sauce BBQ, mozzarella, boeuf, merguez, poulet, swirl de sauce BBQ</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza 3 Fromages</h2>
@@ -58,6 +61,7 @@ function Pizza(params) {
                 <ul>
                     <li>Roquefort, tomate (concentré), mozzarella (boule), chèvre frais</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza 4 Fromages</h2>
@@ -67,6 +71,7 @@ function Pizza(params) {
                 <ul>
                     <li>Mozzarella, gorgonzola (bleu), comté, fromage de chèvre</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza Royale</h2>
@@ -76,6 +81,7 @@ function Pizza(params) {
                 <ul>
                     <li>Tomate, jambon, mozzarella, olives noires</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza Végétarienne</h2>
@@ -85,6 +91,7 @@ function Pizza(params) {
                 <ul>
                     <li>Légumes, pas de viande</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza Provencale</h2>
@@ -94,6 +101,7 @@ function Pizza(params) {
                 <ul>
                     <li>Mozzarella, tomate fraîche, anchois, persillade, olive</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza Italienne</h2>
@@ -103,6 +111,7 @@ function Pizza(params) {
                 <ul>
                     <li>Fromage, lardons, champignons, oeuf, émincés de poulet</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza Espagnole</h2>
@@ -112,6 +121,7 @@ function Pizza(params) {
                 <ul>
                     <li>Chorizo, aubergines, tomates</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza Turque (Pide)</h2>
@@ -121,6 +131,7 @@ function Pizza(params) {
                 <ul>
                     <li>Fromage kasar turc, fromage haloumi, botte de persil</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza Peperoni</h2>
@@ -130,6 +141,7 @@ function Pizza(params) {
                 <ul>
                     <li>Pâte à pizza, sauce tomate, mozzarella, pepperoni</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             <div className="card">
                 <h2>Pizza BBQ</h2>
@@ -139,6 +151,7 @@ function Pizza(params) {
                 <ul>
                     <li>Bacon, poivrons (verts, rouges ou un mélange), coriandre fraîche (hachée), cheddar (en plus de la mozzarella)</li>
                 </ul>
+                <button><i class="fa-solid fa-plus"></i></button><button><i class="fa-solid fa-minus"></i></button><button><i class="fa-solid fa-trash"></i></button>
             </div>
             {/* Ajoutez les cartes suivantes jusqu'au produit 16 */}
         </div>
